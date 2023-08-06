3d0b2908bf57651da2933d4931199eebf65c0f02
Dylan Cross <dylancross100@gmail.com>
2023-08-06 14:23:05 -0500

    cargo init; cargo build; cargo run

---
+++ rust_tutorials/hello_world/target/debug/deps/hello_world-7d3e59caaf88ee3b.d
@@ -0,0 +1,5 @@
+/Users/dylan_cross/Documents/personal/coding/rust/rust_tutorials/hello_world/target/debug/deps/hello_world-7d3e59caaf88ee3b: hello_world.rs
+
+/Users/dylan_cross/Documents/personal/coding/rust/rust_tutorials/hello_world/target/debug/deps/hello_world-7d3e59caaf88ee3b.d: hello_world.rs
+
+hello_world.rs:
---
+++ rust_tutorials/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/dylan_cross/Documents/personal/coding/rust/rust_tutorials/hello_world/target/debug/hello_world: /Users/dylan_cross/Documents/personal/coding/rust/rust_tutorials/hello_world/hello_world.rs
